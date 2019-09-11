88a012cc34837836cfb98dbf91f3d3bc87d9a934
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-11 22:00:33 +0000

    Deploy zero-to-mastery/travel-guide to github.com/zero-to-mastery/travel-guide.git:gh-pages

---
+++ precache-manifest.5f3c74d38e3e391fe404d0b70233f5cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2d09bfeec595f4e4ad9",
+    "url": "/travel-guide/static/css/main.6369d876.chunk.css"
+  },
+  {
+    "revision": "f2d09bfeec595f4e4ad9",
+    "url": "/travel-guide/static/js/main.d197781e.chunk.js"
+  },
+  {
+    "revision": "b1e879db75bb6d189f80",
+    "url": "/travel-guide/static/js/runtime~main.3d26cb24.js"
+  },
+  {
+    "revision": "cb5ac0b7ba906976923c",
+    "url": "/travel-guide/static/css/2.57ecf70d.chunk.css"
+  },
+  {
+    "revision": "cb5ac0b7ba906976923c",
+    "url": "/travel-guide/static/js/2.257596d9.chunk.js"
+  },
+  {
+    "revision": "d8c3acbd31f04801bf24e193425eaf79",
+    "url": "/travel-guide/static/media/Globe.d8c3acbd.svg"
+  },
+  {
+    "revision": "a31ada7f1f2f84b2d4c47abb10419913",
+    "url": "/travel-guide/index.html"
+  }
+];
